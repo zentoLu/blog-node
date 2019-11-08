983140b9f29f8454e70f8bb9f1fb3e3c29b44d6f
zentoLu <491097990@qq.com>
2019-11-08 14:49:55 +0800

    dockerFile

--- app.config.js
+++ app.config.js
@@ -28,7 +28,8 @@ exports.CROSS_DOMAIN = {
 };
 
 exports.MONGODB = {
-	uri: `mongodb://127.0.0.1:${argv.dbport || '27017'}/blogNode`,
+	// uri: `mongodb://127.0.0.1:${argv.dbport || '27017'}/blogNode`,
+	uri: `mongodb://120.24.72.215:${argv.dbport || '27017'}/blogNode`,
 	username: argv.db_username || 'DB_username',
 	password: argv.db_password || 'DB_password',
 };
